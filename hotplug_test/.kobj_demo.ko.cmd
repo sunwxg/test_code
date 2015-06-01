747856bd6b90d8ab3b69277c65a2dab2ebd8349d
Sun Wang <sun.wxg@gmail.com>
2015-06-01 14:52:18 +0800

    add hotplug_test

---
+++ hotplug_test/.kobj_demo.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/home/local/test_code/hotplug_test/kobj_demo.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/home/local/test_code/hotplug_test/kobj_demo.ko /home/home/local/test_code/hotplug_test/kobj_demo.o /home/home/local/test_code/hotplug_test/kobj_demo.mod.o
---
+++ hotplug_test/.tmp_versions/kobj_demo.mod
@@ -0,0 +1,2 @@
+/home/home/local/test_code/hotplug_test/kobj_demo.ko
+/home/home/local/test_code/hotplug_test/kobj_demo.o
