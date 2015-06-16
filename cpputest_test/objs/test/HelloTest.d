6e0448050174d84c0195867940d548c1270c5010
sunwxg <sun.wxg@gmail.com>
2015-06-16 17:03:45 +0800

    cpputest successful

---
+++ cpputest_test/objs/test/HelloTest.d
@@ -0,0 +1,43 @@
+objs/test/HelloTest.o: test/HelloTest.cpp \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/CppUTestConfig.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/StandardCLibrary.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ src/hello.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestHarness.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/Utest.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/SimpleString.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/UtestMacros.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestResult.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestFailure.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestPlugin.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakWarningPlugin.h \
+ /home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakDetectorNewMacros.h
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/CppUTestConfig.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/StandardCLibrary.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+
+src/hello.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestHarness.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/Utest.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/SimpleString.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/UtestMacros.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestResult.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestFailure.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/TestPlugin.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakWarningPlugin.h:
+
+/home/home/ext4local/cpputest-3.7.1/include/CppUTest/MemoryLeakDetectorNewMacros.h:
