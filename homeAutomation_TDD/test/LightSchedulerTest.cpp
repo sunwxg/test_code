ae15c182eb33b14a6855727acffbf8272ba360b9
sunwxg <sun.wxg@gmail.com>
2015-06-18 15:23:38 +0800

    light schedule start

---
+++ homeAutomation_TDD/test/LightSchedulerTest.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "CppUTest/TestHarness.h"
+
+
+TEST_GROUP(LightScheduler)
+{
+
+	void setup()
+	{
+	}
+	
+	void teardown()
+	{
+	}
+
+};
+
+TEST(LightScheduler, PrintOk)
+{
+	FAIL("start here");
+}
