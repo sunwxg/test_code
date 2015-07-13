3f50c85c697625936b6a028120d5fb8ec4442c03
sunwxg <sun.wxg@gmail.com>
2015-07-13 13:38:25 +0800

    add description in list.h

--- data_struct/src/list.h
+++ data_struct/src/list.h
@@ -18,8 +18,24 @@ struct list_ {
 typedef struct list_ LIST;
 
 void list_init(LIST * l);
+
+/* list_add_next
+ * sucess: return 0
+ * fail:   return -1
+ */
 int list_add_next(LIST * l, void *data);
+
+/* list_rm_next
+ * success: return *data of element
+ * fail:    return NULL
+ */
 void * list_rm_next(LIST *l);
+
+
+/* list_rm_element
+ * success: return *data of element
+ * fail:    return NULL
+ */
 void * list_rm_element(LIST *l, struct listElement *element);
 
 #define list_head(list) (list)->head
