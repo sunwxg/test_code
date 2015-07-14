dc54f08f1d4c8ac6951f413b81aa04206e54edbc
sunwxg <sun.wxg@gmail.com>
2015-07-14 16:22:10 +0800

    add bitree find

--- data_struct/src/bitree.c
+++ data_struct/src/bitree.c
@@ -12,61 +12,149 @@ int bitree_destroy(BITREE *b)
 {
 	if (bitree_size(b) > 1)
 		return -1;
-	//free(b->root);
+
+	if (bitree_size(b) == 1)
+		free(b->root);
+
 	memset(b, 0, sizeof(BITREE));
 	return 0;
 }
 
-int bitree_insert_left(BITREE *b, struct bitree_node *node, void *data)
+static struct bitree_node * insert_root_node(BITREE *b, void *data)
+{
+	
+	struct bitree_node *position = calloc(1, sizeof(struct bitree_node));
+
+	if (position == NULL)
+		return NULL;
+
+	position->data = data;
+
+	b->root = position;
+	bitree_size(b)++;
+
+	return position;
+}
+
+struct bitree_node * 
+bitree_insert_left(BITREE *b, struct bitree_node *node, void *data)
 {
 	struct bitree_node *position;
 
-	if (bitree_size(b) == 0) {
-		position = calloc(1, sizeof(struct bitree_node));
-		if (position == NULL)
-			return -1;
-
-		memset(position, 0, sizeof(struct bitree_node));
-		position->data = data;
-
-		b->root = position;
-		bitree_size(b)++;
-		return 0;
-	}
+	if (bitree_size(b) == 0)
+		return insert_root_node(b, data);
 
 	if (node->left != NULL)
-		return -1;
+		return NULL;
 	
 	position = calloc(1, sizeof(struct bitree_node));
 	if (position == NULL)
-		return -1;
+		return NULL;
 
-	memset(position, 0, sizeof(struct bitree_node));
 	position->data = data;
 		
 	node->left = position;
 	bitree_size(b)++;
 
-	return 0;
+	return position;
 }
 
+struct bitree_node * 
+bitree_insert_right(BITREE *b, struct bitree_node *node, void *data)
+{
+	struct bitree_node *position;
+
+	if (bitree_size(b) == 0)
+		return insert_root_node(b, data);
+
+	if (node->right != NULL)
+		return NULL;
+	
+	position = calloc(1, sizeof(struct bitree_node));
+	if (position == NULL)
+		return NULL;
+
+	position->data = data;
+		
+	node->right = position;
+	bitree_size(b)++;
+
+	return position;
+}
 
 void * bitree_remove_left(BITREE *b, struct bitree_node *node)
 {
 	void *data;
-	struct bitree_node *node_left;
+
+	if (node == NULL)
+		return NULL;
 
 	if (bitree_size(b) == 0)
 		return NULL;
 	
-	node_left = node->left;
 	if (!bitree_is_leaf(node->left))
 		return NULL;	
 
 	data = node->left->data;
-	printf("node_left p=%p\n", node_left);
-	free(node_left);
+
+	free(node->left);
+	node->left = NULL;
 	bitree_size(b)--;
 
 	return data;
 }
+
+void * bitree_remove_right(BITREE *b, struct bitree_node *node)
+{
+	void *data;
+
+	if (node == NULL)
+		return NULL;
+
+	if (bitree_size(b) == 0)
+		return NULL;
+	
+	if (node->right == NULL)
+		return NULL;
+
+	if (!bitree_is_leaf(node->right))
+		return NULL;	
+
+	data = node->right->data;
+
+	free(node->right);
+	node->right = NULL;
+	bitree_size(b)--;
+
+	return data;
+}
+
+static struct bitree_node *
+bitree_preorder(BITREE *b, struct bitree_node *node, void *data,
+		int (*compare)(void *d1, void *d2))
+{
+	struct dict *d1 = (struct dict *)node->data;
+	struct bitree_node *temp;
+
+	if (node == NULL)
+		return NULL;
+	
+	printf("node is %d\n", d1->key);
+	if (compare(node->data, data) == 1)
+		return node;
+
+	temp = bitree_preorder(b, node->left, data, compare);
+	if (temp != NULL)
+		return temp;
+
+	temp = bitree_preorder(b, node->right, data, compare);
+
+	return temp;
+}
+
+struct bitree_node * bitree_find(BITREE *b, void *data,
+		int (*compare)(void *data1, void *data2))
+{
+	return bitree_preorder(b, b->root, data, compare);
+}
+
