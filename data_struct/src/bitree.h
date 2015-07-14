3320a53742d6f2382fb04aeb0fd637bc931a357f
sunwxg <sun.wxg@gmail.com>
2015-07-14 15:47:38 +0800

    Revert "preorder susccess"
    This reverts commit deac90150f414cdb0a8766a5f995b3e495286005.

--- data_struct/src/bitree.h
+++ data_struct/src/bitree.h
@@ -9,57 +9,20 @@ struct bitree_node {
 	struct bitree_node *right;
 };
 
-struct bitree_ {
+struct bitree {
 	int size;
 	struct bitree_node *root;
 };
 
-#define BITREE struct bitree_
+#define BITREE struct bitree
 
 void bitree_init(BITREE *b);
-
-/* bitree_destroy
- * success: return 0
- * fail:    return -1
- */
 int bitree_destroy(BITREE *b);
+int bitree_insert_left(BITREE *b, struct bitree_node *node, void *data);
 
-/* bitree_insert_left
- * success: return node pointer
- * fail:    return NULL
- */
-struct bitree_node * 
-bitree_insert_left(BITREE *b, struct bitree_node *node, void *data);
-
-/* bitree_insert_right
- * success: return node pointer
- * fail:    return NULL
- */
-struct bitree_node * 
-bitree_insert_right(BITREE *b, struct bitree_node *node, void *data);
-
-/* bitree_remove_left
- * success: return * data of node
- * fail:    return NULL
- */
 void * bitree_remove_left(BITREE *b, struct bitree_node *node);
-
-/* bitree_remove_right
- * success: return * data of node
- * fail:    return NULL
- */
-void * bitree_remove_right(BITREE *b, struct bitree_node *node);
-
-/* bitree_find
- * success: return node pointer
- * fail:    return NULL
- */
-struct bitree_node * bitree_find(BITREE *b, void *data, int (*compare)(void *data1, void *data2));
-
 
 #define bitree_size(b) ((b)->size)
 #define bitree_is_leaf(node) (((node)->left == NULL) && ((node)->right == NULL))
-#define node_left(node) ((node)->left)
-#define node_right(node) ((node)->right)
 
 #endif /* __BITREE_H */
--- data_struct/test/bitree_test.c
+++ data_struct/test/bitree_test.c
@@ -2,87 +2,36 @@
 #include "bitree.h"
 #include "bitree.c"
 
-struct dict {
+struct bitree_dict {
 	int key;
 	int value;
 };
 
-int compare(void *d1, void *d2)
-{
-	struct dict *a, *b;
-
-	a = d1;
-	b = d2;
-
-	return (a->key == b->key) ? 1 : 0;
-}
-
 static BITREE btree;
-static struct dict d1 = {1, 1};
-static struct dict d2 = {21, 2};
+static struct bitree_dict b_d1 = {1, 1};
+//static struct dict d2 = {21, 2};
 //static struct dict temp = {31, 3};
 
 TEST_GROUP(bitree_test);
 
 TEST_SETUP(bitree_test)
 {
-	bitree_init(&btree);
 }
 
 TEST_TEAR_DOWN(bitree_test)
 {
-	bitree_destroy(&btree);
 }
 
 TEST(bitree_test, bitree_insert_leftTreeSize)
 {
-	bitree_insert_left(&btree, btree.root, (void *)&d1);
-	bitree_insert_left(&btree, btree.root, (void *)&d2);
-
+	TEST_IGNORE();
+	bitree_init(&btree);
+	bitree_insert_left(&btree, btree.root, (void *)&b_d1);
+	bitree_insert_left(&btree, btree.root, (void *)&b_d1);
 	TEST_ASSERT_EQUAL_INT(2, bitree_size(&btree));
 	
-	bitree_remove_left(&btree, btree.root);
-}
-
-TEST(bitree_test, bitree_insert_leftDataValue)
-{
-	bitree_insert_left(&btree, btree.root, (void *)&d1);
-	bitree_insert_left(&btree, btree.root, (void *)&d2);
-
-	TEST_ASSERT_EQUAL_INT(1, ((struct dict *)btree.root->data)->value);
-	TEST_ASSERT_EQUAL_INT(2, ((struct dict *)(btree.root->left->data))->value);
-
-	bitree_remove_left(&btree, btree.root);
-}
-
-TEST(bitree_test, bitree_insert_rightTreeSize)
-{
-	bitree_insert_right(&btree, btree.root, (void *)&d1);
-	bitree_insert_right(&btree, btree.root, (void *)&d2);
-
-	TEST_ASSERT_EQUAL_INT(2, bitree_size(&btree));
-	
-	bitree_remove_right(&btree, btree.root);
-}
-
-TEST(bitree_test, bitree_insert_rightDataValue)
-{
-	bitree_insert_right(&btree, btree.root, (void *)&d1);
-	bitree_insert_right(&btree, btree.root, (void *)&d2);
-
-	TEST_ASSERT_EQUAL_INT(1, ((struct dict *)btree.root->data)->value);
-	TEST_ASSERT_EQUAL_INT(2, ((struct dict *)(btree.root->right->data))->value);
-
-	bitree_remove_right(&btree, btree.root);
-}
-
-TEST(bitree_test, bitree_remove_leftNonData)
-{
-	TEST_ASSERT_EQUAL_PTR(NULL, bitree_remove_left(&btree, btree.root));	
-}
-
-TEST(bitree_test, bitree_remove_rightNonData)
-{
-	TEST_ASSERT_EQUAL_PTR(NULL, bitree_remove_right(&btree, btree.root));	
+	//bitree_remove_left(&btree, btree.root);
+	//TEST_ASSERT_EQUAL_PTR(&d1, bitree_remove_left(&b, b.root));
+	//bitree_destroy(&b);
 }
 
