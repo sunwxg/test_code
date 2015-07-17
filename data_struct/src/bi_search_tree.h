dfff9e4db1d2012951f89184d9d14b9440ad4880
sunwxg <sun.wxg@gmail.com>
2015-07-17 11:58:13 +0800

    bi search tree.h add description

--- data_struct/src/bi_search_tree.h
+++ data_struct/src/bi_search_tree.h
@@ -7,10 +7,22 @@
 
 void bi_search_tree_init(BI_S_TREE *b, int (*compare)(void *d1, void *d2));
 
+/* bi_search_tree_insert
+ * success: return 0
+ * fail   : return -1
+ */
 int bi_search_tree_insert(BI_S_TREE *b, void *data);
 
+/* bi_search_tree_remove
+ * success: return -1
+ * fail   : return 0
+ */
 int bi_search_tree_remove(BI_S_TREE *b, void *data);
 
+/* bi_search_tree_search
+ * success: return pointer of data
+ * fail   : return NULL
+ */
 void * bi_search_tree_search(BI_S_TREE *b, void *data);
 
 #endif /* __BI_SEARCH_TREE_H */
